771805f616f10bf89357fc592ddcf9001cd532c3
翟隐 <884275720@qq.com>
2018-09-25 05:02:40 -0700

    分类列表数据的获取-不包含分页和排序

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+# 获取某个第三级分类的所有商品信息
+# 分页，排序
+
+# GET /categories/(?P<category_id>\d+)/skus/
+from goods.models import SKU
+from goods.serializers import SKUSerializer
+
+
+class SKUListView(ListAPIView):
+    serializer_class = SKUSerializer
+    # queryset = SKU.objects.filter(catogry_id=category_id, islanched=True)
+
+    def get_queryset(self):
+        """指定当前视图所使用的查询集"""
+        category_id = self.kwargs['category_id']
+        return SKU.objects.filter(category_id=category_id, is_launched=True)
+
+
